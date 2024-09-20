536cf27bd0da36b7db47ad83b2b664387b2384a7
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-09-20 14:18:37 +0100

    start wars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,58 @@
 
 const request = require('request');
 
-// Get movie ID from the first argument
+// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
 
-// Define the API URL for the specified movie
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+// Check if the movie ID was passed as an argument
+if (!movieId) {
+  console.log('Please provide a Movie ID.');
+  process.exit(1);
+}
 
-// Make the API request to get the movie data
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+// Define the base URL for the Star Wars API
+const baseUrl = 'https://swapi.dev/api/films/';
 
-  // Parse the response body to JSON
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  // Loop through each character URL to get the name
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
+// Function to fetch character name
+function fetchCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body.name);
       }
-
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
     });
   });
-});
+}
+
+// Function to fetch and print characters from the movie
+function fetchMovieCharacters (movieId) {
+  const movieUrl = `${baseUrl}${movieId}/`;
+
+  request(movieUrl, { json: true }, (err, res, body) => {
+    if (err) {
+      console.error('Error fetching the movie:', err);
+      return;
+    }
+    if (res.statusCode !== 200) {
+      console.log('Movie not found or invalid Movie ID.');
+      return;
+    }
+
+    // Extract the list of character URLs
+    const characterUrls = body.characters;
+
+    // Use Promise.all to fetch all characters in parallel
+    Promise.all(characterUrls.map(fetchCharacterName))
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(err => {
+        console.error('Error fetching characters:', err);
+      });
+  });
+}
+
+// Call the function to fetch movie characters
+fetchMovieCharacters(movieId);
